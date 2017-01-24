e0cbe474554226eb4660ebde5f82a505375c9e2a
Afzal Hossain <afzal@Afzals-iMac.local>
2017-01-24 20:23:52 +0600

    Initial Commit

---
+++ Pokeman Go Finder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pokeman Go Finder
+//
+//  Created by Afzal Hossain on 1/24/17.
+//  Copyright © 2017 Afzal Hossain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
