d7a7a7ee76d6fb384b8e55f04063b62a128cdb9a
Md AfzaL Hossain <aafzal322@gmail.com>
2017-03-19 23:20:58 +0600

    Using Firebase

---
+++ Pods/Target Support Files/Pods-Pokeman Go Finder/Pods-Pokeman Go Finder-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Pokeman_Go_Finder : NSObject
+@end
+@implementation PodsDummy_Pods_Pokeman_Go_Finder
+@end
---
+++ Pods/Target Support Files/Pods-Pokeman Go Finder/Pods-Pokeman Go Finder-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_Pokeman_Go_FinderVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Pokeman_Go_FinderVersionString[];
+
